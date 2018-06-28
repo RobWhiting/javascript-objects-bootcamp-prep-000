14ccee86fd3c267968658f6e26fa6ce19131b8ae
Rob Whiting <rob.t.whiting@gmail.com>
2018-06-28 21:46:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {
+<<<<<<< HEAD
   artistName: 'songTitle',
 }
 
@@ -9,5 +10,16 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
+=======
+  artist: "song",
+}
+
+function updatePlaylist(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function removeFromPlaylist(obj, key) {
+  delete playlist.key;
+>>>>>>> cd43500582af510b54809dbafbc4eff2e64a0118
   return playlist;
 }
